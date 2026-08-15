7383720a5492d7510a988ff09b7187c83bbf02af
Aung-Dev-Hub <aungmyobusiness@gmail.com>
2026-08-15 21:44:12 +0630

    Daily Update

--- Frontend/src/pages/LandingPage/LandingPage.jsx
+++ Frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,13 @@
+import Features from "./components/Features"
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 const LandingPage = () => {
   return (
     <div className="min-h-screen">
       <Header/>
+      <Hero/>
+      <Features/>
     </div>
   )
 }
